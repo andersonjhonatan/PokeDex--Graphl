86434549591b059be10c18a76c498cad201a00e2
Anderson Jhonatan <105141966+andersonjhonatan@users.noreply.github.com>
2026-08-16 15:13:20 -0300

    refactor: corrige queries GraphQL

--- src/graphql/queries/PokemonQueries.ts
+++ src/graphql/queries/PokemonQueries.ts
@@ -1,34 +1,27 @@
 import { gql } from '@apollo/client'
 
-const GET_All_POKEMONS = gql`
-  query GetAllPokemons(
-    $offset: Int
-    $limit: Int
-    $pokemonOrderBy: [pokemon_v2_pokemon_order_by!]
-    $pokemonSpriteOrderBy: [pokemon_v2_pokemonsprites_order_by!]
-  ) {
-    pokemon_v2_pokemon(limit: $limit, offset: $offset, order_by: $pokemonOrderBy) {
-      order
-      name
-      is_default
+const GET_ALL_POKEMONS = gql`
+  query GetAllPokemons($offset: Int = 0, $limit: Int = 151) {
+    pokemon_v2_pokemon(
+      limit: $limit
+      offset: $offset
+      order_by: { id: asc }
+      where: { is_default: { _eq: true } }
+    ) {
       id
+      name
       height
       base_experience
       weight
-
-      pokemon_v2_pokemonsprites(
-        limit: $limit
-        offset: $offset
-        order_by: $pokemonSpriteOrderBy
-      ) {
+      pokemon_v2_pokemonsprites(limit: 1) {
         sprites
       }
-      pokemon_v2_pokemontypes {
+      pokemon_v2_pokemontypes(order_by: { slot: asc }) {
         pokemon_v2_type {
           name
         }
       }
-      pokemon_v2_pokemonabilities {
+      pokemon_v2_pokemonabilities(order_by: { slot: asc }) {
         pokemon_v2_ability {
           name
         }
@@ -39,24 +32,24 @@ const GET_All_POKEMONS = gql`
 
 const GET_POKEMON_BY_NAME = gql`
   query GetPokemonByName($name: String!) {
-    pokemon_v2_pokemon(where: { name: { _ilike: $name } }) {
-      order
-      name
-      is_default
+    pokemon_v2_pokemon(
+      limit: 1
+      where: { name: { _eq: $name }, is_default: { _eq: true } }
+    ) {
       id
+      name
       height
       base_experience
       weight
-
       pokemon_v2_pokemonsprites(limit: 1) {
         sprites
       }
-      pokemon_v2_pokemontypes {
+      pokemon_v2_pokemontypes(order_by: { slot: asc }) {
         pokemon_v2_type {
           name
         }
       }
-      pokemon_v2_pokemonabilities {
+      pokemon_v2_pokemonabilities(order_by: { slot: asc }) {
         pokemon_v2_ability {
           name
         }
@@ -67,36 +60,30 @@ const GET_POKEMON_BY_NAME = gql`
 
 const GET_POKEMON_BY_ID = gql`
   query GetPokemonById($id: Int!) {
-    pokemon_v2_pokemon(where: { id: { _eq: $id } }) {
-      order
-      name
-      is_default
+    pokemon_v2_pokemon(
+      limit: 1
+      where: { id: { _eq: $id }, is_default: { _eq: true } }
+    ) {
       id
+      name
       height
       base_experience
       weight
       pokemon_v2_pokemonsprites(limit: 1) {
         sprites
       }
-      pokemon_v2_pokemontypes {
+      pokemon_v2_pokemontypes(order_by: { slot: asc }) {
         pokemon_v2_type {
           name
         }
       }
-      pokemon_v2_pokemonabilities {
+      pokemon_v2_pokemonabilities(order_by: { slot: asc }) {
         pokemon_v2_ability {
           name
-        }
-      }
-      pokemon_v2_pokemonspecy {
-        pokemon_v2_pokemonevolutions {
-          pokemon_v2_gender {
-            name
-          }
         }
       }
     }
   }
 `
 
-export { GET_All_POKEMONS, GET_POKEMON_BY_NAME, GET_POKEMON_BY_ID }
+export { GET_ALL_POKEMONS, GET_POKEMON_BY_NAME, GET_POKEMON_BY_ID }
